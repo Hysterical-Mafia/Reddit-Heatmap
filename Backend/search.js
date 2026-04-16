82a1f71b3ce67128fd66552f8ba4aae146fd3c38
Hysterical-Mafia <hekmatullahr.hm@gmail.com>
2026-04-16 13:13:25 -0400

    Express to Vercel
    Moved the backend from express to Vercel. Changed the url for the main.js, currently working on the search.js to connect Vercel

---
+++ Backend/search.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+    const keyword = req.query.keyword;
+
+    
+}
---
+++ main.js
@@ -0,0 +1,40 @@
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const output = document.getElementById("output-section");
+
+const status = document.getElementById("status");
+
+function updateOutput(text) {
+    status.textContent = text;
+}
+    searchBtn.addEventListener("click", async function() {
+    const keyword = input.value; 
+
+    if (keyword.trim() == "") {
+        alert("my input has no value");
+        return;
+    }
+    updateOutput("Searching for: " + keyword);
+    
+    const url = "/api/search?keyword=" + keyword;
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    const posts = data.data.children;
+    output.innerHTML = "";
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        const title = post.data.title;
+
+        const newDiv = document.createElement("div");
+        newDiv.id = "item" + i ;
+        newDiv.className = "post";
+        newDiv.textContent = title;
+
+        output.appendChild(newDiv);
+    }
+
+});
+
+
+
